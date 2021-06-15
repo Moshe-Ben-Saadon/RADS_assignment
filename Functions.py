02397348818ae1a291eb895c849dfd31ae7dc1fe
Moshe-Ben-Saadon <bensaadon8@gmail.com>
2021-06-15 21:51:42 +0300

    Data retrieval function

--- Functions.py
+++ Functions.py
@@ -1,5 +1,16 @@
 import pymongo
 
-
+def data_retrieval(size, database):
+    counter = 0
+    data = list()
+    list_of_chunks = list()
+    for x in database.find().sort("data", pymongo.ASCENDING):
+        data.append(x["data"])
+        counter += 1
+        if counter == (size - 1):
+            list_of_chunks.append(data)
+            data = []
+            counter = 0
+    return list_of_chunks
 
 
